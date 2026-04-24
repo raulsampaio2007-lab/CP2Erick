7893ce4a1fb35c2056754a6551a76e4d13561cc5
pedrosampaiom2007-gif <pedrosampaiom2007@gmail.com>
2026-04-24 12:00:53 -0300

    Organiza prototipos de funcoes e finaliza interface do sistema

--- src/main.c
+++ src/main.c
@@ -1,25 +1,10 @@
 #include <stdio.h>
 
-void inserirNotas(float *n1, float *n2)
-{
 
-    printf("Digite a nota 1: ");
-
-    scanf("%f", n1);
-
-    printf("Digite a nota 2: ");
-
-    scanf("%f", n2);
-
-    printf("Notas salvas!\n");
-}
-
-float calcularMedia(float n1, float n2)
-{
-
-    return (n1 + n2) / 2.0;
-}
-
+void inserirNotas(float *n1, float *n2);
+float calcularMedia(float n1, float n2);
+void verificarSituacao(float media);
+void calcularDerivada();
 int main()
 {
     int opcao;
